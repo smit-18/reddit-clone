cdbe7478d2b1027b861e4b28f69a394c4ed4aaa4
Smit Vekaria <smitvekaria@Smits-MBP.lan>
2024-05-31 11:18:15 -0400

    set up recoil and added basic auth modal

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,37 @@
+import { authModalState } from "@/atoms/authModalAtom";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Here is a Modal Body</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,7 +1,11 @@
+import { authModalState } from "@/atoms/authModalAtom";
 import { Button } from "@chakra-ui/react";
 import React from "react";
+import { useSetRecoilState } from "recoil";
 
 const AuthButtons: React.FC = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <>
       <Button
@@ -10,7 +14,7 @@ const AuthButtons: React.FC = () => {
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "100px" }}
         mr={2}
-        // onClick={() => {}}
+        onClick={() => setAuthModalState({ open: true, view: "login" })}
       >
         Log In
       </Button>
@@ -19,7 +23,7 @@ const AuthButtons: React.FC = () => {
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "100px" }}
         mr={2}
-        // onClick={() => {}}
+        onClick={() => setAuthModalState({ open: true, view: "signup" })}
       >
         Sign Up
       </Button>
