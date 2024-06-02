d4bd9d0a85cbf8e93effc920d982429aa73cf0cb
Smit Vekaria <smitvekaria@Smits-MBP.lan>
2024-06-01 23:39:02 -0400

    added directory to the navbar and built the create community modal interface

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,17 +4,25 @@ import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/clientApp";
+import Directory from "./Directory/Directory";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
 
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justify={{ md: "space-between" }}
+    >
       <Flex align="center">
         <Image
           alt="Reddit Face Logo"
           src="/images/redditFace.svg"
           height="30px"
+          width={{ base: "40px", md: "auto" }}
+          mr={{ base: 0, md: 2 }}
         />
         <Image
           alt="Reddit Text Logo"
@@ -23,9 +31,9 @@ const Navbar: React.FC = () => {
           display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      {/* <Directory /> */}
-      <SearchInput />
-      <RightContent user={user}/>
+      {user && <Directory />}
+      <SearchInput user={user} />
+      <RightContent user={user} />
     </Flex>
   );
 };
