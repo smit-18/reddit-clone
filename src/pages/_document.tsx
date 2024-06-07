24ac5cc25934a4c8d2db65e9e1519e3f0b1adfd0
Smit Vekaria <smitvekaria@Smits-MBP.lan>
2024-06-07 18:45:50 -0400

    updated the _document file to have metadata and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="Reddit Clone">
+        <link rel="icon" href="images/redditFace.svg" type="image/x-icon" />
+        <link
+          rel="shortcut icon"
+          href="images/redditFace.svg"
+          type="image/x-icon"
+        />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="This is a Reddit Clone" />
+        <meta
+          name="keywords"
+          content="React, TypeScript, Reddit, Clone, Next.js, ChakraUI, Firebase"
+        />
+        <meta name="author" content="Smit Vekaria" />
+      </Head>
       <body>
         <Main />
         <NextScript />
